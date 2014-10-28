d9bdb14e5253ccabf49652901114f48d6bb583ae
nkvenom <venom_neko@yahoo.com>
2014-10-28 00:20:19 -0500

    Get rid of unused functions in tuitelerias.py

--- tuitelerias.py
+++ tuitelerias.py
@@ -21,39 +21,3 @@ def get_auth(conf_path='auth_twitter.conf'):
 
     return auth
 
-
-def get_users(f_name='filtered_users_2.json'):
-    """ Extrae unicamente el json que corresponde a los usuarios del archivo de twitts """
-    f = codecs.open(f_name, 'r', 'utf-8')
-    counts = defaultdict(int)
-    users = {}
-
-    # line number
-    ln = 0
-    for line in f:
-        try:
-            ln += 1
-            user = json.loads(line)
-            u_name = user[u'screen_name']
-            counts[u_name] += 1
-
-            if not user['id'] in users:
-                users[user['id']] = user
-        except:
-            print "Unexpected error: %s, at line %d" % (sys.exc_info()[0], ln)
-
-    return users
-
-
-def sort_users_file():
-    users = get_users()
-    user_list = sorted(users.values(), key=lambda user: user['id'])
-    for v in user_list[:10]:
-        print "%d[%s]: %s" % (v['id'], v['screen_name'], v['description'])
-    out = codecs.open('filtered_users_2.json', 'w', 'utf-8')
-    for u in user_list:
-        out.write("%s\n" % json.dumps(u))
-    out.close()
-
-if __name__ == '__main__':
-    my_auth = get_auth()
