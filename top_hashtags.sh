ac550198e7e43605eb39c6f67bfc8cb5e6db1d5b
nkvenom <venom_neko@yahoo.com>
2014-10-30 00:50:28 -0500

    Modificado top_hashtags para solo mostrar los 11 más importantes

--- top_hashtags.sh
+++ top_hashtags.sh
@@ -5,4 +5,4 @@ py --ji -x "x['text']" |
 py -x "x if '#' in x else None" | 
 py -x "'\n'.join([j.lower() for j in x.split() if j.startswith('#')])" | 
 py -x "'#'+''.join(ch for ch in x if ch.isalnum())" | 
-sort | uniq -c | sort -nr
+sort | uniq -c | sort -nr | head -n11
